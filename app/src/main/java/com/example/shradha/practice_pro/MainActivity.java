51343e92b68e621c8c2d26e4d7d30247a15b3f75
saurabh-solkar16 <saurabhsolkar4@gmail.com.com>
2018-08-16 22:29:41 +0530

    second commit login app example

--- app/src/main/java/com/example/shradha/practice_pro/MainActivity.java
+++ app/src/main/java/com/example/shradha/practice_pro/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.shradha.practice_pro;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /*
@@ -19,15 +24,24 @@ Steps to create Activities
   4) create method as given below and assign on click event to it
  */
 public class MainActivity extends AppCompatActivity {
-
+Button button;
+EditText u;
+EditText p;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        u=(EditText) findViewById(R.id.u);
+        p=(EditText)findViewById(R.id.p);
     }
 
     public void func(View v){
+        Toast.makeText(getApplication().getBaseContext(),u.getText()+" "+p.getText(),Toast.LENGTH_LONG).show();
+if (p.getText().toString().equals("admin")&&u.getText().toString().equals("admin")){
         Intent intent=new Intent(".second_activity");
         startActivity(intent);
+    }else {
+    Toast.makeText(getApplication().getBaseContext(),"username or password is wrong",Toast.LENGTH_SHORT).show();
+    }
     }
 }
